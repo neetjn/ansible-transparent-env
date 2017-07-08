c2f6dc16b3c4fe3ddd22a2fa83ad036894db7b2f
John Nolette <john@neetgroup.net>
2017-07-08 16:17:20 -0400

    Added VSCode extensions

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -6,26 +6,3 @@ if ! which code; then
   sudo dpkg --install code.deb
   rm -rf code.deb
 fi;
-
-# -- install vscode extensions
-code --install-extension EditorConfig.EditorConfig
-code --install-extension abusaidm.html-snippets
-code --install-extension arcticicestudio.nord-visual-studio-code
-code --install-extension christian-kohler.npm-intellisense
-code --install-extension christian-kohler.path-intellisense
-code --install-extension crisward.riot-tag
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension donjayamanne.python
-code --install-extension dzannotti.vscode-babel-coloring
-code --install-extension eg2.vscode-npm-script
-code --install-extension file-icons.file-icons
-code --install-extension formulahendry.terminal
-code --install-extension gerane.Theme-azure
-code --install-extension hnw.vscode-auto-open-markdown-preview
-code --install-extension jasonnutter.search-node-modules
-code --install-extension liuji-jim.vue
-code --install-extension marcostazi.VS-code-vagrantfile
-code --install-extension octref.vetur
-code --install-extension waderyan.nodejs-extension-pack
-code --install-extension xabikos.JavaScriptSnippets
-code --install-extension zhuangtongfa.Material-theme
